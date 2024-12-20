72918a43310d63dd92a061e04da4eb692678e8f0
Ido Ben-Hur <idoybh2@gmail.com>
2024-12-20 19:12:34 +0200

    Settings: Hide EID number in about phone
    prevents leaking private info in screenshots by mistake
    inline with what we did all across this page...
    the dialog already shows EID so no reason to show it in the summary
    
    Change-Id: I73f949089e3a306ed1d21c3f323ac1e3559ed558

--- src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceController.kt
+++ src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceController.kt
@@ -96,7 +96,6 @@ class SimEidPreferenceController(context: Context, preferenceKey: String) :
             }
             preference.title = title
             preference.dialogTitle = title
-            preference.summary = eid
             updateDialog()
         }
     }
